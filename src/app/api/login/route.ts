3655cfa6895b30eb4ebc1cf9e89c21dff567c605
migueww <miguew.png@gmail.com>
2025-12-16 01:12:48 -0300

    feat login page

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -24,7 +24,9 @@ export async function POST(request: Request) {
   response.cookies.set("auth", "1", {
     httpOnly: true,
     sameSite: "lax",
+    secure: process.env.NODE_ENV === "production",
     path: "/",
+    maxAge: 60 * 60 * 24 * 7, // 7 dias
   });
 
   return response;
